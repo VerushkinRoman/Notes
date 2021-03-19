2f7be99076cc793316e0288c29ce9b487acc7d77
Posse <posse66666@gmail.com>
2021-03-19 15:43:57 +0300

    Added_side_panel

---
+++ app/src/main/java/com/posse/android1/notes/ui/settings/SettingsFragment.java
@@ -0,0 +1,28 @@
+package com.posse.android1.notes.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.posse.android1.notes.R;
+
+public class SettingsFragment extends Fragment {
+
+    private SettingsViewModel settingsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        final TextView textView = root.findViewById(R.id.text_gallery);
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
+        return root;
+    }
+}
